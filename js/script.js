01d3ab7049e1d175a9b67635eccd34e646e60440
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-02-05 18:03:12 +0300

    feature: landing-page- add a restart button

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 
 let score = 0;
-var reTake = document.getElementsByClassName('redo')
+
 
 const quiz = document.getElementById("form1");
 
@@ -13,15 +13,17 @@ e.preventDefault();//prevent refresh of the page everytime the submit btn is cli
 
 
 let answer = document.quiz.question1.value;// geting the value of each correct answer
-let answer2= document.quiz.question2.value
+let answer2= document.quiz.question2.value;
+let answer3= document.quiz.question3.value;
+let answer4 = document.quiz.question4.value;
 
 
+if(answer == "C"){ score ++};
 
+if(answer2 == "A"){ score ++};
+if(answer3 == "B"){ score ++};
 
-if(answer == "C"){ score ++;}
-
-if(answer2 == "A"){ score ++;}
-
+if (answer4 == "A"  && answer4 == "D"){ console.log('het');};
 
 quiz.style.display ="none"; // to display the score set form display to none
 
