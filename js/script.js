c3794504140a6771b60abb754490e6221b2636ea
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-02-05 13:58:03 +0300

    rf:create-new landing page- Create a new landing page

--- js/script.js
+++ js/script.js
@@ -1,15 +1,42 @@
 
+
 let score = 0;
+var reTake = document.getElementsByClassName('redo')
 
 const quiz = document.getElementById("form1");
 
-var result = document.getElementById("result");
-quiz.addEventListener("submit" , (e) =>{
+const result = document.getElementById("result");//displays results
+let startQuiz = quiz.addEventListener("submit" , (e) =>{  
+  
 e.preventDefault();//prevent refresh of the page everytime the submit btn is clicked
-var answer = document.quiz.question1.value
-if(answer == "scripting language"){
-    score ++;
-    
-}
-result.textContent= score;
+;
+
+
+let answer = document.quiz.question1.value;// geting the value of each correct answer
+let answer2= document.quiz.question2.value
+
+
+
+
+if(answer == "C"){ score ++;}
+
+if(answer2 == "A"){ score ++;}
+
+
+quiz.style.display ="none"; // to display the score set form display to none
+
+total();
 });
+
+
+function total(){
+
+    if (score< 2){
+        result.textContent="Your score  is:  " + score  +" " + "try bit harder nexttime";
+    }else{
+        result.textContent=  "Your score is:  " + score + " "+ "congratulation";
+    } ;
+
+  
+};
+
